3a59bb3e0cc0035cc9c1cf97f0da3d9d250da22a
unknown <1135802188.com>
2020-03-19 23:38:50 +0800

    提交第四次作业

---
+++ Homework4/HomeWork4_1/HomeWork4_1/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork4_1
+{
+    // 链表节点
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+
+    }
+
+    //泛型链表类
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void ForEach(Action<T> action)
+        {
+
+            Node<T> node = head;
+            while (node != null)
+            {
+                action(node.Data);
+                node = head.Next;
+            }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 整型List
+            GenericList<int> list = new GenericList<int>();
+            for (int n = 0; n < 10; n++)
+            {
+                list.Add(n);
+            }
+            list.ForEach(n => Console.WriteLine(n));
+            int min = -1;
+            list.ForEach(n => { if (n < min) min = n; });
+            Console.WriteLine($"最小值是:{min}");
+            int max = 20;
+            list.ForEach(n => { if (n > max) max = n; });
+            Console.WriteLine($"最大值是：{max}");
+            int sum = 0;
+            list.ForEach(n => sum += n);
+            Console.WriteLine($"总和为：{sum}");
+
+
+        }
+    }
+}
---
+++ Homework4/HomeWork4_2/HomeWork4_2/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork4_2
+{
+    //声明委托类型
+    public delegate void ClockHandler(object sender,RunEventArgs arg);
+
+    public class RunEventArgs {
+        public int Hour{ get; set; }
+        public int Minute { get; set; }
+    }
+
+    public class Clock
+    {
+        public event ClockHandler Tick, Alarm;
+        //设置滴答
+        public void clockTick(int hour, int minute)
+        {
+            RunEventArgs args1 = new RunEventArgs()
+            {
+                Hour = hour,
+                Minute = minute
+            };
+            Console.WriteLine($"{hour}:{minute}");
+            Tick(this, args1);
+        }
+        //设置闹钟
+        public void clockAlarm(int hour,int minute) {
+
+            System.DateTime currentTime = new DateTime();
+            currentTime = System.DateTime.Now;
+            int currentHour = currentTime.Hour;
+            int currentMinute = currentTime.Minute;
+
+            RunEventArgs args2 = new RunEventArgs() {
+                Hour=hour,
+                Minute=minute
+            };
+            Console.WriteLine($"闹钟为{hour}:{minute}");
+
+            if (currentHour == hour && currentMinute == minute)
+            {
+                Alarm(this, args2);
+            }
+            else {
+                Console.WriteLine($"现在是{currentTime},未到设置闹钟时间");
+            }
+
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Clock clock = new Clock();
+
+            clock.Tick += new ClockHandler(tick);
+            clock.Alarm += alarm;
+
+            void tick(object sender,RunEventArgs arg) {
+                System.DateTime dateTime = new DateTime();
+                dateTime = System.DateTime.Now;
+                Console.WriteLine($"时间为{dateTime}");
+            }
+
+            void alarm(object sender,RunEventArgs arg) {
+
+            }
+
+            clock.clockTick(23, 13);
+            clock.clockAlarm(23,01);
+        }
+    }
+}
